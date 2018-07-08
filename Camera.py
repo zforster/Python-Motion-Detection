11366735ed03bdae5e1677a4a0ff73427f590706
zforster <zf31@kent.ac.uk>
2018-07-08 19:32:04 +0000

    changed camera variable name to webcam to stop error

--- Camera.py
+++ Camera.py
@@ -4,7 +4,11 @@ class Camera:
     def __init__(self):
         pygame.camera.init()
         self.cam_list = pygame.camera.list_cameras()
-        self.camera = self.cam_list[0]
-        self.camera.start()
+        self.webcam = self.cam_list[0]
+        self.webcam.start()
+
+    def get_image(self):
+        return self.camera.get_image()
 
 cam = Camera()
+cam.get_image()
